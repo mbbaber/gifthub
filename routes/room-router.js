16e6ec6d521eeb187140c9b3f4a56908e7e69ccf
Maggie Baber <mbbaber@gmail.com>
2018-04-23 18:03:09 +0200

    made popup buttons, started my-room view, created routes for room and rooms

--- routes/room-router.js
+++ routes/room-router.js
@@ -6,45 +6,83 @@ const router = express.Router();
 
 const Room = require("../models/room-model")
 
-
-
 // ROUTES
 ///////////////////////////////////////////////////////
+
+//  This is my route to individual group page
+// router.get('/groups/:groupId', (req, res, next) => {
+//     .then(data => {
+//         res.locals.groupArray = data.body.items;
+//         res.render('room-views/my-room');
+//     })
+//      .catch(err) => {
+//          console.log("You have an error", err);
+//     })
+// })
 
 // render rooms-list page
 router.get("/my-rooms", (req, res, next) => {
     res.render("rooms-list");
-    //Room.find({member: req.user._id }) //will find only the rooms whose user is the logged-in user.
-    //.populate("member")
-    //.then((roomsFromDb) => {
-    //    res.locals.roomList = roomsFromDb;
-    //    res.render("rooms-list");
-    // })
-    // .catch((err) => {
-    //     next(err);
-    // })
 });
+//     Room.find({member: req.user._id }) //will find only the rooms whose user is the logged-in user.
+//     .populate("member")
+//     .then((roomsFromDb) => {
+//         res.locals.roomList = roomsFromDb;
+//         res.render("rooms-list");
+//     })
+//     .catch((err) => {
+//         next(err);
+//     })
+// });
 
+//CREATE A NEW ROOM/GROUP IN THE DATABASE
 router.post("/process-room", (req, res, next) => {
-    if (!req.user) {
-        res.flash("error", "You must be logged-in to see this page")
-        res.redirect("/");
-        return
-    }
+    // if (!req.user) {
+    //     res.flash("error", "You must be logged-in to see this page")
+    //     res.redirect("/login");
+    //     return
+    // }
     const { name, description, pictureUrl } = req.body;
     //creates room for whomever is logged-in
-    Room.create({ name, description, pictureUrl, owner: req.user._id })
+    const administratorId = req.user._id;
+    Room.create({ name, description, pictureUrl: administratorId})
+
+    //Room.create({name, description, pictureUrl})
     .then(() => {
-        //res.flash("success", "Room created!");
-        res.redirect("/");
+        console.log("success Room created!");
+        res.redirect("/my-rooms");
     })
     .catch((err) => {
         next(err);
     })
 });
 
-///////////////////////////////////////////////////////
-
-
+//INVITE FRIENDS/PARTICIPANTS
+router.post('/process-invite', (req, res, next) => {
+    const {sender, senderEmail, message} = req.body;
+    transport.sendMail({
+      from: "Your website <website@example.com",
+      to: process.env.gmail_user, 
+      subject: `${sender} is trying to contact you`,
+      text: `
+        Name: ${sender}
+        Email: ${senderEmail}
+        Message: ${message}
+      `,
+      html: `
+      <h1>Contact Form Message<h1>
+      <p>Name: <b>${sender}<br></p>
+      <p>Email: ${senderEmail}</p>
+      <p>Message: ${message}</p>
+      `
+    })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => {
+      next(err)
+    })
+    //res.send(req.body); was only for testing purposes
+  });
 
 module.exports = router;
