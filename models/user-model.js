f787d62ca5db4e750127df2229f5a4c9d06b5a5b
Lucas M <montenoise.lucas@gmail.com>
2018-04-23 15:21:04 +0200

    Got signup to work

--- models/user-model.js
+++ models/user-model.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema ({
     fullName: {type: String, required: true },
     email: {type: String, required: true, unique: true },
-    pictureUrl: {type: String, required: true },
+    pictureUrl: String,
     // normal sign up & login
     encryptedPassword: {type: String, required: true },
     roomsList: [
