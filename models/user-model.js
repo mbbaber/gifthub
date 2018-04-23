723034ca920d67970043add273a05df1a723a46b
Lucas M <montenoise.lucas@gmail.com>
2018-04-23 15:37:11 +0200

    added padding to the main display

--- models/user-model.js
+++ models/user-model.js
@@ -13,7 +13,7 @@ const userSchema = new Schema ({
     roomsList: [
         {
         type: Schema.Types.ObjectId,
-        unique: true,
+        // unique: true,
         ref: "Room"
         }
     ]
